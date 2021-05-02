cbbfc799c401f5b79b0cd3cce1bece5e2087b254
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 19:49:38 -0400

    added content to while loop, it works!

--- while-loop.sh
+++ while-loop.sh
@@ -2,3 +2,10 @@
 
 # Read through a file line by line
 
+LINE=1
+
+while read -r CURRENT_LINE #creating new variable to use
+    do
+        echo "$LINE: $CURRENT_LINE" # this actually going to be the text of the line
+        ((LINE++)) #for iteration
+done < "new-1.txt" # < "(name of the file)"
