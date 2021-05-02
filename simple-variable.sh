5044232838580d2d9ef2b47948784f6b35de229c
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 19:05:29 -0400

    added second file

---
+++ simple-variable.sh
@@ -0,0 +1,12 @@
+#! usr/bin/bash
+
+# VARIABLE
+
+# Uppercase by convention, Letters, numbers amd underscores are allowed
+
+NAME="EREN"
+
+# SPACES MATTER SO DON'T DO NAME = "Eren" <-- Won't WORK!!
+
+echo "My name is $NAME"
+echo "My name is ${NAME}" # <-- just like in template literalls in JS
