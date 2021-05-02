7839dcb472ff897799e7a6378db9fdf310879f93
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 19:54:05 -0400

    added basic commands

---
+++ basic-commands.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+# Create Folder and Write to a file
+
+mkdir hello
+
+touch "hello/world.txt"
+
+echo "Hello World!" >> "hello/world.txt" #to add to the file use >> and the name of the tile
+
+echo "Created hello/world.txt"
