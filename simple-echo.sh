2d7908007e2422baa9a04eef73db642d161be7d5
Yigit Ozgur <yigiterenozgur@outlook.com>
2021-05-02 19:03:38 -0400

    added first file

---
+++ simple-echo.sh
@@ -0,0 +1,14 @@
+# # = called sharp, ! = called a bang, #! = called schbang for including the bash
+# use which bash to find where your bash located
+
+# make the file readable and enable permissions by using change mode: 
+
+# chmod +x (name of the file) simple-echo.sh <-- makes it executable with permission
+
+# to run the program with bash use ./ (filename) eg. ./simple-echo.sh
+
+#! usr/bin/bash
+
+# ECHO Command
+
+echo Hello World!
